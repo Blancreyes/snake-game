35e2467dae4f91854c86bad8ea259904b901a105
Blancreyes <luijbr83@gmail.com>
2023-03-27 10:41:23 +0000

    Second Commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,21 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const playBoard = document.querySelector(".playboard");
+
+let foodX = 13,
+  foodY = 10;
+
+const foodPosition = () => {
+  foodX = Math.floor(Math.random() * 30) + 1;
+  foodY = Math.floor(Math.random() * 30) + 1;
 };
+
+const initGame = () => {
+  let htmlMarkup = `<div class="food" style="grid-area:${foodY}/${foodX}"></div>`;
+  playBoard.innerHTML = htmlMarkup;
+};
+
+initGame();
